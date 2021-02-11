fe486adc8d13e4af4499b6ca63881cd2c7ae134c
cternes <github@slackspace.de>
2021-02-11 14:57:47 +0100

    Solve day 7 part I

---
+++ day7/app.test/Parser/RuleInterpreterTest.cs
@@ -0,0 +1,33 @@
+﻿namespace app.test.Parser
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using app.Parser;
+    using FluentAssertions;
+    using Xunit;
+
+    public class RuleInterpreterTest
+    {
+        [Fact]
+        public void ShouldWork()
+        {
+            // Arrange
+            var rules = new List<string>
+            {
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags."
+            };
+            var interpreter = new RuleInterpreter();
+
+            // Act
+            var bags = interpreter.InterpretRules(rules);
+
+            // Assert
+            var redBag = bags.Single(i => i.Name == "light red");
+            var yellowBag = redBag.Children.Single(i => i.Name == "muted yellow");
+            
+            yellowBag.Children.Should().Contain(i => i.Name == "shiny gold");
+            yellowBag.Children.Should().Contain(i => i.Name == "faded blue");
+        }
+    }
+}
---
+++ day7/app/Model/Bag.cs
@@ -0,0 +1,15 @@
+﻿namespace app.Model
+{
+    using System.Collections.Generic;
+
+    public class Bag
+    {
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public List<Bag> Children { get; set; } = new List<Bag>();
+        
+        public bool IsOuterBag { get; set; }
+    }
+}
